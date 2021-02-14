7cbd3bd15aa26f4ceb0a121f8fb9f76eb88bf3f3
Chad Carbert <chadcarbert@me.com>
2021-02-13 20:44:19 -0500

    Migrate Occurrence styles to tailwind

--- components/rhythm/occurrence.js
+++ components/rhythm/occurrence.js
@@ -1,35 +1,34 @@
-import styles from "../../styles/Occurrence.module.css";
 import { format } from 'date-fns';
 
 export default function Occurrence({ hit, onClick, open, date }) {
-  const classKeys = ["occurrence"];
-
-  if (open) {
-    classKeys.push(hit ? "open-hit" : "open-miss");
-  } else {
-    classKeys.push(hit ? "hit" : "miss");
-  }
-
-  const classNames = classKeys.map((key) => styles[key]).join(" ");
-
+  const classNames = ["w-6", "h-6", "rounded-full", "inline-block"];
   const formattedDate = format(date, "MMMM do, yyyy");
-
 
   if (open) {
     const ariaLabel = hit ? `Mark as missed on ${formattedDate}` : `Mark as hit on ${formattedDate}`;
+    classNames.push("bg-white");
+
+    if (!hit) {
+      classNames.push("filter-blur-occurrence-miss");
+    }
 
     return (
       <button
         role="button"
         aria-label={ariaLabel}
         onClick={onClick}
-        className={classNames}
+        className={classNames.join(' ')}
       ></button>
     );
   } else {
+    classNames.push(hit ? "bg-red-600" : "bg-gray-100");
+
     const ariaLabel = hit
       ? `Hit target on ${formattedDate}`
       : `Missed target on ${formattedDate}`;
-    return <div role="img" aria-label={ariaLabel} className={classNames}></div>;
+
+    return (
+      <div role="img" aria-label={ariaLabel} className={classNames.join(' ')}></div>
+    );
   }
 }
