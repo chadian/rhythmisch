104380f947f9d25a9e49d174046cbb35dbbb95f1
Chad Carbert <chadcarbert@me.com>
2021-02-13 21:02:19 -0500

    Add edit-rhythm acceptance test

---
+++ __tests__/edit-rhythm.test.js
@@ -0,0 +1,40 @@
+import RhythmischApp from "../pages/index";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+
+beforeEach(() => {
+  render(<RhythmischApp />);
+});
+
+it("can edit an existing rhythm", () => {
+  const editButton = screen.getByRole('button', { name: 'Edit' });
+  userEvent.click(editButton);
+
+  const modal = screen.getByRole("dialog");
+  expect(modal).toBeInTheDocument();
+
+  const action = screen.getByLabelText("Rhythm action description");
+  const numeratorFrequency = screen.getByLabelText("Rhythm action count");
+  const denomenatorFrequency = screen.getByLabelText(
+    "Rhythm action count time span"
+  );
+  const reason = screen.getByLabelText("Rhythm reason description");
+  const submit = screen.getByRole("button", { name: "Update" });
+
+  userEvent.clear(action);
+  userEvent.type(action, "read every day");
+  userEvent.selectOptions(numeratorFrequency, "thrice");
+  userEvent.selectOptions(denomenatorFrequency, "week");
+  userEvent.clear(reason);
+  userEvent.type(reason, "there is much I would like to learn");
+  userEvent.click(submit);
+
+  expect(modal).not.toBeInTheDocument();
+
+  expect(screen.getByText("Read every day")).toBeInTheDocument();
+  expect(screen.getByText("thrice every week")).toBeInTheDocument();
+  expect(
+    screen.getByText("because there is much I would like to learn")
+  ).toBeInTheDocument();
+});
