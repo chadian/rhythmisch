a9e9b5e2797165522e708ad1327277ccd32e0972
Chad Carbert <chadcarbert@me.com>
2021-02-15 12:41:22 -0500

    Clean up storing of hit dates with local storage

--- hooks/rhythms.js
+++ hooks/rhythms.js
@@ -1,42 +1,71 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { formatISO, parseISO } from "date-fns";
 import { nanoid } from "nanoid";
 
 const RHYTHMS_LOCAL_STORAGE_KEY = "app.rhythms";
-
-const defaultRhythm = {
-  id: nanoid(),
-  action: "Use Rhythmisch",
-  frequency: [1, 1],
-  reason: "I want to get into the rhythm",
-  hits: [new Date("January 6, 2021")],
-};
-
 const RhythmsContext = createContext();
+
+function getLocalStorageRhythms() {
+  try {
+    const jsonData = window.localStorage.getItem(RHYTHMS_LOCAL_STORAGE_KEY);
+    let rhythms = JSON.parse(jsonData);
+
+    if (!Array.isArray(rhythms)) {
+      return;
+    }
+
+    rhythms = rhythms.map((rhythm) => {
+      rhythm.hits = rhythm.hits.map(parseISO);
+      return rhythm;
+    });
+
+    return rhythms;
+  } catch {
+    return;
+  }
+}
+
+function setLocalStorageRhythms(rhythms) {
+  rhythms = rhythms.map(rhythm => {
+    rhythm.hits = rhythm.hits.map(formatISO);
+    return rhythm;
+  });
+
+  window.localStorage.setItem(
+    RHYTHMS_LOCAL_STORAGE_KEY,
+    JSON.stringify(rhythms)
+  );
+}
+
 
 export const RhythmsProvider = ({ children }) => {
   let rhythms = [];
-  let setRhythms = () => {};
 
   // with the `window` object we're in the browser
   // otherwise we're it's being SSR'd
   // TODO: Turn off SSR for what ends up being the
   // app route
   if (typeof window !== 'undefined') {
-    const localRhythms = JSON.parse(
-      window.localStorage.getItem(RHYTHMS_LOCAL_STORAGE_KEY)
-    ) || [defaultRhythm];
+    const defaultRhythm = {
+      id: nanoid(),
+      action: "Use Rhythmisch",
+      frequency: [1, 1],
+      reason: "I want to get into the rhythm",
+      hits: [new Date("January 6, 2021")],
+    };
 
-    const [rhythmsState, setRhythmsState] = useState(localRhythms);
-    rhythms = rhythmsState;
-
-    setRhythms = (rhythms) => {
-      setRhythmsState(rhythms);
-      window.localStorage.setItem(RHYTHMS_LOCAL_STORAGE_KEY, JSON.stringify(rhythms));
-    }
+    rhythms = getLocalStorageRhythms() || [defaultRhythm];
   }
 
+  const [, setRhythmsState] = useState(rhythms);
+
+  const setRhythms = (rhythms) => {
+    setRhythmsState(rhythms);
+    setLocalStorageRhythms(rhythms);
+  };
+
   return (
-    <RhythmsContext.Provider value={[rhythms ?? [], setRhythms ?? (() => {})]}>
+    <RhythmsContext.Provider value={[rhythms, setRhythms]}>
       {children}
     </RhythmsContext.Provider>
   );
