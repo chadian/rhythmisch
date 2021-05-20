a40dc1c0e575daa8ae07468d3453e228b162eb07
Chad Carbert <chadcarbert@me.com>
2021-05-20 13:13:43 -0700

    Add storybook and dependencies

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "stories": [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+}
