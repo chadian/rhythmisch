00e878b1d64483c4f05e2d56732a4763c501c00b
Chad Carbert <chadcarbert@me.com>
2022-02-09 19:02:47 -0500

    Apply types to changed tsx files

--- components/content/section.tsx
+++ components/content/section.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import { ReactChildrenProps } from '../../types';
 
-export function SectionHeading({ children }) {
+export function SectionHeading({ children }: ReactChildrenProps) {
   return <h3 className="text-3xl text-gray-800 font-bold mb-4">{children}</h3>;
 }
 
-export function SectionParagraph({ children }) {
+export function SectionParagraph({ children }: ReactChildrenProps) {
   return <p className="text-2xl text-gray-700">{children}</p>;
 }
