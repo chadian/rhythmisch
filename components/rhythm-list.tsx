84ad92fbd2477d45d802a87233445a950358a702
Chad Carbert <chadcarbert@me.com>
2022-02-10 05:19:17 -0500

    Rename rhythm-list.js -> tsx

--- components/rhythm-list.tsx
+++ components/rhythm-list.tsx
@@ -3,7 +3,6 @@ import Rhythm from './rhythm/rhythm';
 import Button from './button';
 import { useRhythms } from '../hooks/rhythms';
 
-// eslint-disable-next-line react/prop-types
 export default function RhythmList({ onEdit }) {
   const [rhythms, rhythmsDispatch] = useRhythms();
 
