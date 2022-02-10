e6f4c579233180279ae26d1a687b24284240b025
Chad Carbert <chadcarbert@me.com>
2022-02-09 19:14:23 -0500

    Fix tailwind config purge settings to include ts and tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,11 +42,11 @@ function addColorCssProperties({ addComponents, config }) {
 
 module.exports = {
   purge: [
-    './pages/**/*.js',
-    './components/**/*.js',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
     './components/**/*.mdx',
-    './hooks/**/*.js',
-    './utils/**/*.js',
+    './hooks/**/*.{js,jsx,ts,tsx}',
+    './utils/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
