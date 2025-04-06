865abb55aa6289b3e42dbde18223780bce444b06
Chad <2770198+chadian@users.noreply.github.com>
2025-04-06 14:16:59 -0400

    Make changes to fix build command

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,8 @@ import react from 'eslint-plugin-react';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import tseslint from 'typescript-eslint';
 import eslint from '@eslint/js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default defineConfig([
   globalIgnores(['out/**/*', '.next', '.storybook']),
